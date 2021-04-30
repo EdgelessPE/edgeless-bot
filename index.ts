21c713f18016272f53183ba8eb43c6014a034ea7
Cno <dsyourshy@qq.com>
2021-04-30 15:20:33 +0800

    完成任务处理函数

--- index.ts
+++ index.ts
@@ -106,6 +106,87 @@ function log(text:string){
             console.log(text)
     }
 }
+async function getMD5(filePath:string):Promise<string> {
+    return new Promise(resolve => {
+        let rs = fs.createReadStream(filePath)
+        let hash = cpt.createHash('md5')
+        let hex
+        rs.on('data', hash.update.bind(hash))
+        rs.on('end', function () {
+            hex = hash.digest('hex')
+            log('Info:MD5 is ' + hex)
+            resolve(hex)
+        })
+    })
+}
+function parseDownloadUrl(href:string):string {
+    //识别根目录字符“/”
+    if(href[0]==="/") href="https://portableapps.com"+href
+
+    return encodeURI(href)
+}
+function matchVersion(text:string):Interface {
+    let regex=/\d+.\d+(.\d+)*/
+    let matchRes=text.match(regex)
+    if(!matchRes||matchRes.length===0){
+        return new Interface({
+            status:Status.ERROR,
+            payload:"Warning:Matched nothing when looking into \""+text+"\" with \""+regex+"\",skipping..."
+        })
+    }else if(matchRes.length>1){
+        log("Warning:Matched more than 1 result when looking into \""+text+"\" with \""+regex+"\"")
+    }
+    return new Interface({
+        status:Status.SUCCESS,
+        payload:matchRes[0]
+    })
+} //Interface:string
+function versionCmp(a:string,b:string):Cmp {
+    let x=a.split(".")
+    let y=b.split(".")
+    let result:Cmp=Cmp.E
+
+    for (let i=0;i<Math.min(x.length,y.length);i++){
+        if(Number(x[i])<Number(y[i])){
+            result=Cmp.L
+            break
+        }else if(Number(x[i])>Number(y[i])){
+            result=Cmp.G
+            break
+        }
+    }
+    //处理前几位版本号相同但是位数不一致的情况，如1.3/1.3.0
+    if(result===Cmp.E&&x.length!==y.length){
+        //找出较长的那一个
+        let t:Array<string>
+        t = x.length < y.length ? y : x
+        //读取剩余位
+        for (let i=Math.min(x.length,y.length);i<Math.max(x.length,y.length);i++){
+            if(Number(t[i])!==0){
+                result= x.length < y.length ? Cmp.L : Cmp.G
+                break
+            }
+        }
+    }
+
+    return result
+}
+function removeExtraBuilds(database:DatabaseNode,repo:string):DatabaseNode{
+    //builds降序排列
+    database.builds.sort((a,b)=>{
+        return 1-versionCmp(a.version,b.version)
+    })
+    //删除多余的builds
+    for(let i=0;i<database.builds.length-MAX_BUILDS;i++){
+        let target=database.builds.pop()
+        log("Info:Remove extra build "+repo+"/"+target.name)
+        fs.unlinkSync(repo+"/"+target.name)
+    }
+
+    return database
+}
+
+//init
 function beforeRunCheck():boolean{
     //预设严重错误函数
     let l=function (text:string):boolean{
@@ -128,37 +209,26 @@ function beforeRunCheck():boolean{
         }
     })
     //检查命令可用性
-    let cmdList:Array<string> = ["scp","wget"]
-    cmdList.forEach((cmd)=>{
+    let cmdList:Array<any> = [
+        {
+            cmd:"scp",
+            hint:"openssh"
+        },
+        {
+            cmd:"wget",
+            hint:"wget"
+        }
+    ]
+        cmdList.forEach((item)=>{
         try{
-            cp.execSync("where "+cmd)
+            cp.execSync("where "+item.cmd)
         }catch (err) {
-            return l("Command not found:"+cmd)
+            return l("Command not found:"+item.cmd+",please install "+item.hint+"\nTry \"scoop install "+item.hint+"\" if you have scoop installed")
         }
     })
 
     return true
 }
-async function getMD5(filePath:string):Promise<string> {
-    return new Promise(resolve => {
-        let rs = fs.createReadStream(filePath)
-        let hash = cpt.createHash('md5')
-        let hex
-        rs.on('data', hash.update.bind(hash))
-        rs.on('end', function () {
-            hex = hash.digest('hex')
-            log('Info:MD5 is ' + hex)
-            resolve(hex)
-        })
-    })
-}
-function parseDownloadUrl(href:string):string {
-    //识别根目录字符“/”
-    if(href[0]==="/") href="https://portableapps.com"+href
-
-    return encodeURI(href)
-}
-//Interface:string
 function find7zip():Interface {
     let possiblePath=["C:\\Program Files\\7-Zip\\7z.exe","C:\\Program Files (x86)\\7-Zip\\7z.exe",'7zz.exe',"7z.exe","7za.exe",]
     let result=null
@@ -179,7 +249,7 @@ function find7zip():Interface {
             payload:result
         })
     }
-}
+} //Interface:string
 function cleanWorkshop():boolean {
     let dst=DIR_WORKSHOP
     if(fs.existsSync(dst)){
@@ -193,6 +263,8 @@ function cleanWorkshop():boolean {
     fs.mkdirSync(dst)
     return fs.existsSync(dst)
 }
+
+//database
 function readDatabase():any {
     let dst=PATH_DATABASE
     if(!fs.existsSync(dst)){
@@ -210,6 +282,8 @@ function saveDatabase(json:any) {
 
     fs.writeFileSync(dst,JSON.stringify(json))
 }
+
+//task
 function getTasks():Array<string> {
     let dst=DIR_TASKS
     let fileList=fs.readdirSync(dst)
@@ -219,7 +293,6 @@ function getTasks():Array<string> {
     })
     return result
 }
-//Interface:Task
 function readTaskConfig(name:string):Interface {
     let dir=DIR_TASKS+"/"+name
 
@@ -234,7 +307,7 @@ function readTaskConfig(name:string):Interface {
     //解析Json
     let json=JSON.parse(fs.readFileSync(dir+"/config.json").toString())
 
-    //检查Json文件健全性
+    //检查Json健全性
     let miss=null
     for (let taskKey in new Task()) {
         if(!json[taskKey]||json[taskKey]===null){
@@ -253,41 +326,7 @@ function readTaskConfig(name:string):Interface {
         status:Status.SUCCESS,
         payload:json
     })
-}
-//Interface:string
-function matchVersion(text:string):Interface {
-    let regex=/\d+.\d+(.\d+)*/
-    let matchRes=text.match(regex)
-    if(!matchRes||matchRes.length===0){
-        return new Interface({
-            status:Status.ERROR,
-            payload:"Warning:Matched nothing when looking into \""+text+"\" with \""+regex+"\",skipping..."
-        })
-    }else if(matchRes.length>1){
-        log("Warning:Matched more than 1 result when looking into \""+text+"\" with \""+regex+"\"")
-    }
-    return new Interface({
-        status:Status.SUCCESS,
-        payload:matchRes[0]
-    })
-}
-function versionCmp(a:string,b:string):Cmp {
-    let x=a.split(".")
-    let y=b.split(".")
-    let result:Cmp=Cmp.E
-
-    for (let i=0;i<Math.min(x.length,y.length);i++){
-        if(Number(x[i])<Number(y[i])){
-            result=Cmp.L
-            break
-        }else if(Number(x[i])>Number(y[i])){
-            result=Cmp.G
-            break
-        }
-    }
-
-    return result
-}
+} //Interface:Task
 async function getWorkDirReady(name:string,url:string,p7zip:string,md5:string,req:Array<string>):Promise<boolean> {
     let dir=DIR_WORKSHOP+"/"+name
 
@@ -365,21 +404,6 @@ function runMakeScript(name:string):boolean {
 
     return true
 }
-function removeExtraBuilds(database:DatabaseNode,repo:string):DatabaseNode{
-        //builds降序排列
-        database.builds.sort((a,b)=>{
-            return 1-versionCmp(a.version,b.version)
-        })
-        //删除多余的builds
-        for(let i=0;i<database.builds.length-MAX_BUILDS;i++){
-            let target=database.builds.pop()
-            log("Info:Remove extra build "+repo+"/"+target.name)
-            fs.unlinkSync(repo+"/"+target.name)
-        }
-
-        return database
-}
-//Interface:DatabaseNode
 function buildAndDeliver(name:string,version:string,author:string,category:string,p7zip:string,database:DatabaseNode):Interface{
     let serverIP="pineapple.edgeless.top"
     let serverPort="1000"
@@ -395,7 +419,7 @@ function buildAndDeliver(name:string,version:string,author:string,category:strin
     if(!fs.existsSync(dir+"/"+zname)){
         return new Interface({
             status:Status.ERROR,
-            payload:"Warning:Compress "+zname+" failed,skipping..."
+            payload:"Error:Compress "+zname+" failed,skipping..."
         })
     }
     //移动至编译仓库
@@ -426,9 +450,10 @@ function buildAndDeliver(name:string,version:string,author:string,category:strin
         status:Status.SUCCESS,
         payload:database
     })
-}
+} //Interface:DatabaseNode
 
-//scraper:PageInfo
+//scraper
+//Interface:PageInfo
 async function scrapePage(url):Promise<Interface>{
     //配置可识别的类名
     let validClassName=[".download-link",".download-info"]
@@ -511,14 +536,72 @@ async function scrapePage(url):Promise<Interface>{
 }
 
 //task processor
-// async function processTask(task:Task):Promise<Interface> {
-//
-// }
+//Interface:DatabaseNode
+async function processTask(task:Task,database:DatabaseNode,p7zip:string):Promise<Interface> {
+    //抓取页面信息
+    let iScrape=await scrapePage(task.paUrl)
+    if(iScrape.status===Status.ERROR) {
+        log(iScrape.payload)
+        return new Interface({
+            status:Status.ERROR,
+            payload:"Warning:Can't scrape "+task.name+" 's page,skipping..."
+        })
+    }
+    let pageInfo=iScrape.payload as PageInfo
+
+    //匹配版本号
+    let iVersion=matchVersion(pageInfo.text)
+    if(iVersion.status===Status.ERROR){
+        log(iVersion.payload)
+        return new Interface({
+            status:Status.ERROR,
+            payload:"Warning:Can't match "+task.name+" 's version from page,skipping..."
+        })
+    }
+    let version=iVersion.payload as string
+
+    //与数据库进行校对
+    let ret:Interface
+    switch(versionCmp(database.latestVersion,version)){
+        case Cmp.L:
+            //需要升级
+            if(!await getWorkDirReady(task.name,pageInfo.href,p7zip,"",task.requirement)){
+                ret = new Interface({
+                    status:Status.ERROR,
+                    payload:"Warning:Can't get "+task.name+" 's workshop ready,skipping..."
+                })
+                break
+            }
+            if(!runMakeScript(task.name)){
+                ret = new Interface({
+                    status:Status.ERROR,
+                    payload:"Warning:Can't run "+task.name+" 's make script,skipping..."
+                })
+                break
+            }
+            ret = new Interface({
+                status:Status.SUCCESS,
+                payload:buildAndDeliver(task.name,version,task.author,task.category,p7zip,database).unwarp()
+            })
+            break
+        case Cmp.G:
+            //本地大于在线，异常
+            log("Warning:"+task.name+"'s local version is greater than online version,local="+database.latestVersion+",online="+version)
+            ret = new Interface({
+                status:Status.SUCCESS,
+                payload:database
+            })
+            break
+        default:
+            //已是最新版本，不需要操作
+            log("Info:"+task.name+" has been up to date,local="+database.latestVersion+",online="+version)
+            ret = new Interface({
+                status:Status.SUCCESS,
+                payload:database
+            })
+            break
+    }
+    return ret
+}
 
 //main
-// scrapePage("https://portableapps.com/apps/music_video/potplayer-portable")
-// .then((res)=>{
-//     let pageInfo=res.unwarp() as PageInfo
-//     console.log(pageInfo.text)
-//     console.log(pageInfo.href)
-// })

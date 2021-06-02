cfba17d3c135d9705495727c54c4dd8c06f67ad6
Cno <dsyourshy@qq.com>
2021-06-03 03:59:53 +0800

    增加MAX_BUILDS最小值校验

--- src/const.ts
+++ src/const.ts
@@ -9,12 +9,12 @@ export const _userConfig = new UserConfig(
 // 远程开关
 export const ENABLE_REMOTE = args.hasOwnProperty("d")?false:_userConfig.resolved.enableRemote;
 // 忽略远程警告
-export const IGNORE_REMOTE = args.hasOwnProperty("d")?true:_userConfig.resolved.ignoreRemote;
+export const IGNORE_REMOTE = args.hasOwnProperty("d")?false:_userConfig.resolved.ignoreRemote;
 
 export const DIR_TASKS = _userConfig.resolved.dirTask;
 export const DIR_WORKSHOP = _userConfig.resolved.dirWorkshop;
 export const DIR_BUILDS = _userConfig.resolved.dirBuilds;
 export const PATH_DATABASE = _userConfig.resolved.pathDatabase;
-export const MAX_BUILDS = _userConfig.resolved.maxBuildsNum;
+export const MAX_BUILDS = Math.max(_userConfig.resolved.maxBuildsNum,1);
 export const REMOTE_NAME = _userConfig.resolved.remoteName;
 export const REMOTE_ROOT = _userConfig.resolved.remoteRoot;

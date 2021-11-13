a205ec07436f7b09934258b3e0ed1049bbd033c8
Cno <dsyourshy@qq.com>
2021-11-13 19:20:12 +0800

    修复wireshark

--- make.cmd
+++
@@ -1,2 +0,0 @@
-move .\release .\build\Wireshark_bot
-echo echo LINK X:\Users\Default\Desktop\Wireshark,X:\Program Files\Edgeless\Wireshark_bot\WiresharkPortable64.exe >./build/Wireshark_bot.wcs
---
+++ tasks/Wireshark/make.cmd
@@ -0,0 +1,3 @@
+move .\release .\build\Wireshark_bot
+echo "FILE X:\Program Files\Edgeless\Wireshark_bot->X:\Users\PortableApps\Wireshark_bot" >./build/Wireshark_bot.wcs
+echo LINK X:\Users\Default\Desktop\Wireshark,X:\Users\PortableApps\Wireshark_bot\WiresharkPortable64.exe >>./build/Wireshark_bot.wcs

c803b44599f901c3fe8a939fd5701502270239f3
Hydrogen <3038094028@qq.com>
2021-12-04 22:41:39 +0800

    fix: use builtin release installer

--- tasks/balenaEtcher/make.cmd
+++ tasks/balenaEtcher/make.cmd
@@ -1,7 +1,3 @@
 @echo off
-if not exist ".\release\$PLUGINSDIR\app-64.7z" (
-  echo.not found app archive
-  exit /b 9009
-)
 
 "%p7zip%" x ".\release\$PLUGINSDIR\app-64.7z" -o.\build\balenaEtcher -y
--- tasks/balenaEtcher/scraper.ts
+++ tasks/balenaEtcher/scraper.ts
@@ -8,40 +8,43 @@ interface ReleaseEntry {
     url: string;
 }
 
-let entry: ReleaseEntry;
+let entry: ReleaseEntry = {
+    version: 'v1.7.1',
+    url: 'https://github.com/balena-io/etcher/releases/download/v1.7.1/balenaEtcher-Portable-1.7.1.exe'
+  };
 
 export async function init() {
-    const resp = await axios.get<any[]>(FETCH_URL)
-    const latest = resp?.data?.[0]
-    if (latest == null) {
-        throw new Error("fetch releases error!")
-    }
+    // const resp = await axios.get<any[]>(FETCH_URL)
+    // const latest = resp?.data?.[0]
+    // if (latest == null) {
+    //     throw new Error("fetch releases error!")
+    // }
 
-    let version = latest.name as string
-    version = version.trim().slice(
-        version.indexOf('v')
-    )
+    // let version = latest.name as string
+    // version = version.trim().slice(
+    //     version.indexOf('v')
+    // )
 
-    let asset_item: any
+    // let asset_item: any
 
-    for (const item of latest.assets) {
-        if (ASSET_PATTERN.test(item.name)) {
-            asset_item = item
-            break;
-        }
-    }
+    // for (const item of latest.assets) {
+    //     if (ASSET_PATTERN.test(item.name)) {
+    //         asset_item = item
+    //         break;
+    //     }
+    // }
 
-    if (asset_item == null) {
-        throw new Error("find asset error!")
-    }
+    // if (asset_item == null) {
+    //     throw new Error("find asset error!")
+    // }
 
-    const url = asset_item.browser_download_url.trim()
+    // const url = asset_item.browser_download_url.trim()
 
-    entry = {
-        version,
-        url
-    }
-    console.log(entry)
+    // entry = {
+    //     version,
+    //     url
+    // }
+    // console.log(entry)
     return;
 }  
 

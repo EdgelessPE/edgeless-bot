a1cda87983bb3456df7ecd7c5d477e6d1d74ff21
Cno <dsyourshy@qq.com>
2021-12-27 22:33:44 +0800

    完成类定义

---
+++ src/class.ts
@@ -0,0 +1,130 @@
+//爬虫模板
+enum ValidationType {
+  "MD5",
+  "CRC32",
+  "SHA-1",
+}
+interface ScraperParameters {
+  url: string;
+  downloadLinkRegex: string;
+  versionMatchRegex?: string;
+}
+interface ScraperReturned {
+  version: string;
+  downloadLink: string;
+  validation?: {
+    type: ValidationType;
+    value: string;
+  };
+}
+interface ScraperRegister{
+    name:string
+    urlRegex:string
+}
+
+//云盘下载模板
+interface ResolverParameters{
+    downloadLink:string
+    fileMatchRegex:string
+    cd?:Array<string>
+}
+interface ResolverReturned{
+    directLink:string
+}
+interface ResolverRegister{
+    name:string
+    urlRegex:string
+}
+
+//自动制作模板
+interface ProducerParameters{
+    requiredObject:any
+    deleteList?:Array<string>
+}
+interface ProducerReturned{
+    path:string
+}
+interface ProducerRegister{
+    name:string
+    description:string
+}
+
+//数据库
+interface BuildStatus{
+            version:string
+        timestamp:string
+        fileName:string
+}
+interface DatabaseNode{
+    taskName:string
+    recent:{
+        //健康度，0-3
+        health:number
+        errorMessage:string
+        builds:Array<BuildStatus>
+    }
+}
+
+//任务实例
+interface TaskInstance{
+    name:string
+    pageUrl:string
+    options:{
+        requireWindows:boolean
+        missingVersionTask:boolean
+    }
+    template:{
+        scraper?:string
+        resolver?:string
+        producer:string
+    }
+    downloadLinkRegex:string
+    versionMatchRegex?:string
+    fileMatchRegex:string
+    cd?:Array<string>
+    producerRequiredObject:any
+    buildManifest:Array<string>
+    compressLevel?:number
+}
+
+//程序行为控制参数
+interface BehaviorController{
+    DATABASE_UPDATE:boolean
+    DATABASE_PATH:string
+
+    REMOTE_ENABLE:boolean
+    REMOTE_NAME:string
+    REMOTE_PATH:string
+
+    DIR_TASKS:string
+    DIR_WORKSHOP:string
+    DIR_BUILDS:string
+
+    MAX_BUILDS:number
+    MAX_RETRY_SCRAPER:number
+    MAX_RETRY_RESOLVER:number
+    MAX_RETRY_PRODUCER:number
+
+    ARIA2_SPAWN:boolean
+    ARIA2_PORT:string
+    ARIA2_SECRET:string
+
+    SPECIFY_TASK:string
+    MODE_FORCED:boolean
+    GITHUB_ACTIONS:boolean
+}
+
+export {
+    ScraperParameters,
+    ScraperReturned,
+    ScraperRegister,
+    ResolverParameters,
+    ResolverReturned,
+    ResolverRegister,
+    ProducerParameters,
+    ProducerReturned,
+    ProducerRegister,
+    DatabaseNode,
+    TaskInstance,
+    BehaviorController
+}

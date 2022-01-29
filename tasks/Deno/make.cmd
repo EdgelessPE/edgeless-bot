a6a482727c10dbf091d9a7a37a4a5ffa672e39ed
Hydrogen <3038094028@qq.com>
2022-01-29 13:17:08 +0800

    feat(deno): some bugs (#19)
    * feat: add `Deno` task
    
    * chore: debug
    
    * Update main.yml
    
    * chore: remove debug
    
    * Update config.json
    
    * fix: remove debug
    
    * fix: wrong output path

--- tasks/Deno/make.cmd
+++ tasks/Deno/make.cmd
@@ -1,8 +1,7 @@
 @echo off
 mkdir ".\build\deno"
 move ".\release\deno.exe" ".\build\deno\deno.exe"
-".\build\deno\deno.exe" types --unstable
-call ".\build\deno\deno.exe" types --unstable > ".\build\lib.deno.d.ts"
-call ".\build\deno\deno.exe" -V > ".\build\VERSION"
+call ".\build\deno\deno.exe" types --unstable > ".\build\deno\lib.deno.d.ts"
+call ".\build\deno\deno.exe" -V > ".\build\deno\VERSION"
 
 

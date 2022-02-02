bb804c5926369d006c2d5de884cb8ab508eb6eb7
Cno <dsyourshy@qq.com>
2022-02-03 00:33:34 +0800

    增加酷我音乐（无版本号任务）

---
+++ tasks/酷我音乐/scraper.ts
@@ -0,0 +1,27 @@
+import {robustGet} from '../../src/network';
+import {Ok, Result} from 'ts-results';
+import {ScraperReturned} from '../../src/class';
+
+let version: string,
+	url: string;
+
+async function init() {
+	url = 'https://down.kuwo.cn/mbox/kwmusic_web_4.exe';
+	version = '0';
+}
+
+function getVersion(): string {
+	return version;
+}
+
+function getDownloadLink(): string {
+	return url;
+}
+
+export default async function (): Promise<Result<ScraperReturned, string>> {
+	await init();
+	return new Ok({
+		version: getVersion(),
+		downloadLink: getDownloadLink(),
+	});
+}

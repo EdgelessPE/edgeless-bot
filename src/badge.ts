fe2e1219081ff12a6fcc59bd455edc02de1088e2
董晓天 <34956162+XiaotianDong@users.noreply.github.com>
2022-02-04 23:54:14 +0800

    重写getBadge
    改的稍微短了点

--- src/badge.ts
+++ src/badge.ts
@@ -5,53 +5,75 @@ let count = 0;
 
 export function getBadge(type: string): string {
 	let res;
-	switch ((base + count) % 14) {
-		case 0:
-			res = chalk.bgRed(type + ' ' + count);
-			break;
-		case 1:
-			res = chalk.bgGreen(type + ' ' + count);
-			break;
-		case 2:
-			res = chalk.bgYellow(type + ' ' + count);
-			break;
-		case 3:
-			res = chalk.bgBlue(type + ' ' + count);
-			break;
-		case 4:
-			res = chalk.bgMagenta(type + ' ' + count);
-			break;
-		case 5:
-			res = chalk.bgCyan(type + ' ' + count);
-			break;
-		case 6:
-			res = chalk.bgWhite(type + ' ' + count);
-			break;
-		case 7:
-			res = chalk.bgGray(type + ' ' + count);
-			break;
-		case 8:
-			res = chalk.bgRedBright(type + ' ' + count);
-			break;
-		case 9:
-			res = chalk.bgGreenBright(type + ' ' + count);
-			break;
-		case 10:
-			res = chalk.bgYellowBright(type + ' ' + count);
-			break;
-		case 11:
-			res = chalk.bgBlueBright(type + ' ' + count);
-			break;
-		case 12:
-			res = chalk.bgMagentaBright(type + ' ' + count);
-			break;
-		case 13:
-			res = chalk.bgCyanBright(type + ' ' + count);
-			break;
-		default:
-			res = chalk.bgWhiteBright(type + ' ' + count);
-			break;
-	}
+	let choices = [
+		chalk.bgRed,
+		chalk.bgGreen,
+		chalk.bgYellow,
+		chalk.bgBlue,
+		chalk.bgMagenta,
+		chalk.bgCyan,
+		chalk.bgWhite,
+		chalk.bgGray,
+		chalk.bgRedBright,
+		chalk.bgGreenBright,
+		chalk.bgYellowBright,
+		chalk.bgBlueBright,
+		chalk.bgMagentaBright,
+		chalk.bgCyanBright
+	];
+	let choice = (base + count) % 14
+	res = choice <= 13 && choice >= 0 ? choices[choice](type + ' ' + count) : chalk.bgWhiteBright(type + ' ' + count);
+
 	count++;
+
 	return res;
 }
+
+
+	//switch ((base + count) % 14) {
+	//	case 0:
+	//		res = chalk.bgRed(type + ' ' + count);
+	//		break;
+	//	case 1:
+	//		res = chalk.bgGreen(type + ' ' + count);
+	//		break;
+	//	case 2:
+	//		res = chalk.bgYellow(type + ' ' + count);
+	//		break;
+	//	case 3:
+	//		res = chalk.bgBlue(type + ' ' + count);
+	//		break;
+	//	case 4:
+	//		res = chalk.bgMagenta(type + ' ' + count);
+	//		break;
+	//	case 5:
+	//		res = chalk.bgCyan(type + ' ' + count);
+	//		break;
+	//	case 6:
+	//		res = chalk.bgWhite(type + ' ' + count);
+	//		break;
+	//	case 7:
+	//		res = chalk.bgGray(type + ' ' + count);
+	//		break;
+	//	case 8:
+	//		res = chalk.bgRedBright(type + ' ' + count);
+	//		break;
+	//	case 9:
+	//		res = chalk.bgGreenBright(type + ' ' + count);
+	//		break;
+	//	case 10:
+	//		res = chalk.bgYellowBright(type + ' ' + count);
+	//		break;
+	//	case 11:
+	//		res = chalk.bgBlueBright(type + ' ' + count);
+	//		break;
+	//	case 12:
+	//		res = chalk.bgMagentaBright(type + ' ' + count);
+	//		break;
+	//	case 13:
+	//		res = chalk.bgCyanBright(type + ' ' + count);
+	//		break;
+	//	default:
+	//		res = chalk.bgWhiteBright(type + ' ' + count);
+	//		break;
+	//}

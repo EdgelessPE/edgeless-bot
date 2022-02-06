801d2edbcd3f884f8cc5880225858e5f049d5ab0
Cno <dsyourshy@qq.com>
2022-02-07 01:10:25 +0800

    完成producer_required简易输入器

--- scripts/new.ts
+++ scripts/new.ts
@@ -5,6 +5,8 @@ import chalk from 'chalk';
 import {CATEGORIES} from '../src/const';
 import fs from 'fs';
 import producerRegister from '../templates/producers/_register';
+import path from 'path';
+const TOML = require('@iarna/toml')
 
 interface TaskInput {
 	task: TaskConfig['task'],
@@ -20,6 +22,17 @@ interface TaskInput {
 	producer_required: any
 }
 
+type SchemaType="string"|"array"|"integer"|"object"
+
+interface Schema {
+	properties:{
+		[key:string]:{
+			type:SchemaType
+		}
+	},
+	required:string[]
+}
+
 function printHelp() {
 	//展示帮助信息
 	console.log('');
@@ -30,27 +43,76 @@ function printHelp() {
 }
 
 async function createTask() {
+	let producerEntrance=""
+	const inputProducer=async ()=>{
+		let index=await select('制作器模板', (() => {
+			let r: string[] = [];
+			producerRegister.forEach((item) => {
+				r.push(chalk.bgBlueBright('name') + ' ' + item.name
+					+ '\n' + chalk.bgCyanBright('description') + ' ' + item.description,
+				);
+			});
+			return r;
+		})())
+		producerEntrance=producerRegister[index].entrance
+		return producerEntrance
+	}
+	const generateProducerRequired=async ():Promise<string>=>{
+		const schemaFilePath = path.join('./schema/producer_templates', producerEntrance + '.json')
+		const schemaJson=JSON.parse(fs.readFileSync(schemaFilePath).toString()) as Schema
+		let t:SchemaType,resJson:any={},tmp
+		for(let key of schemaJson.required){
+			t=schemaJson.properties[key].type
+			switch (t) {
+				case 'array':
+					tmp=await input(`自动制作模板要求的${chalk.cyan("数组")}参数：${key},使用 ' 包裹单个元素,使用 , 分割,不需要加中括号`,"",/('[^',]+'\s*,)*\s*('[^',]+')+/)
+					resJson[key]=tmp.replace(/'/g,"").split(",")
+					break
+				case 'integer':
+					resJson[key]=Number(await input(`自动制作模板要求的${chalk.cyan("整数")}参数：${key}`,undefined,/^[0-9]+$/))
+					break
+				case 'object':
+					if(resJson["producer_required"]==undefined) resJson["producer_required"]={}
+					tmp=await input(`自动制作模板要求的${chalk.cyan("对象")}参数：${key},需要加花括号`,undefined,/{.*}/)
+					try{
+						resJson["producer_required"][key]=JSON.parse(tmp)
+					}catch (e) {
+						console.log(JSON.stringify(e,null,2));
+						log("Error:Can't parse input as object, please modify toml config later manually")
+						resJson["producer_required"][key]={}
+					}
+					break
+				case 'string':
+					resJson[key]=await input(`自动制作模板要求的${chalk.cyan("字符串")}参数：${key}`)
+					break
+				default:
+					log(`Error:Unimplemented type ${t}, please modify toml config later manually`)
+					resJson[key]=""
+
+			}
+		}
+		return TOML.stringify(resJson)
+	}
 	let json = {
-		// task:{
-		// 	name:await input("任务名称"),
-		// 	category:CATEGORIES[await select("任务分类",CATEGORIES)],
-		// 	author:await input("作者"),
-		// 	url:await input("上游URL",undefined,/^https?:\/\//)
-		// },
-		template: {
-			producer: producerRegister[await select('制作器模板', (() => {
-				let r: string[] = [];
-				producerRegister.forEach((item) => {
-					r.push(chalk.bgBlueBright('name') + ' ' + item.name
-						+ '\n' + chalk.bgCyanBright('description') + ' ' + item.description,
-					);
-				});
-				return r;
-			})())].entrance,
+		task:{
+			name:await input("任务名称"),
+			category:CATEGORIES[await select("任务分类",CATEGORIES)],
+			author:await input("作者"),
+			url:await input("上游URL",undefined,/^https?:\/\//)
 		},
+		template: {
+			producer: await inputProducer(),
+		},
+		regex:{
+			download_name:await input("下载文件校验正则","/.exe/",/^\/.+\/$/)
+		},
+		parameter:{
+			build_manifest:await input("构建装箱单,使用 ' 包裹单个文件，使用 , 分割	","'${taskName}.wcs','${taskName}'",/('[^',]+'\s*,)*\s*('[^',]+')+/)
+		},
+		producer_required:await generateProducerRequired()
 	};
-	let toml = fs.readFileSync('./scripts/templates/task.toml').toString();
-	console.log(applyInput(toml, json, '').unwrap());
+	let taskToml = fs.readFileSync('./scripts/templates/task.toml').toString();
+	console.log(applyInput(taskToml, json, '').unwrap());
 }
 
 async function createTemplate() {

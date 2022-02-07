fffcb5c2e34eea1f0b0901be7d1939c7e5fbb284
undefined <34956162+XiaotianDong@users.noreply.github.com>
2022-02-07 09:49:49 +0800

    Change Path of link in make.cmd

--- tasks/Atom/make.cmd
+++ tasks/Atom/make.cmd
@@ -2,4 +2,4 @@
 
 ::移动
 move /y ".\release\Atom x64" ".\build\Atom_bot"
-echo LINK X:\Users\Default\Desktop\Atom,X:\Users\PortableApps\Atom_bot\Atom.exe >>./build/Atom_bot.wcs
+echo LINK X:\Users\Default\Desktop\Atom,X:\Program Files\Edgeless\Atom_bot\Atom.exe >>./build/Atom_bot.wcs

1b3dd03b1f7dc4dbae6851936ce60d5744391c5d
dongxiaotian <dongxiaotian123456@outlook.com>
2022-02-08 00:47:40 +0800

    add Notepad3

---
+++ tasks/Notepad3/make.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+::移动
+move /y ".\release" ".\build\Notepad3_bot"
+echo link "X:\Users\Default\Desktop\Notepad3","X:\Program Files\Edgeless\Notepad3_bot\Notepad3.exe" >> .\build\Notepad3_bot.wcs

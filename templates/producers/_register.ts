1c90ff0240878083a3fb16e73dad3c5e6b15e308
Cno <dsyourshy@qq.com>
2022-02-08 21:35:31 +0800

    修改推荐装箱单

--- templates/producers/_register.ts
+++ templates/producers/_register.ts
@@ -5,14 +5,14 @@ const regArray: Array<ProducerRegister> = [
 		name: 'Click to Install',
 		entrance: 'Click2Install',
 		description: 'Create a shortcut on the desktop to allow user\'s manually install or click to run;' +
-			'\nSuggested buildManifest : [\'${taskName}.wcs\',\'${taskName}/${downloadedFile}\']',
+			'\nSuggested buildManifest : ${taskName}.wcs,${taskName}/${downloadedFile}',
 		defaultCompressLevel: 1,
 	},
 	{
 		name: 'Recursive Unzip',
 		entrance: 'Recursive_Unzip',
 		description: 'Recursive unzip downloaded file by array "recursiveUnzipList", then create a shortcut on the desktop for "sourceFile";' +
-			'\nSuggested buildManifest : [\'${taskName}.wcs\',"sourceFile"]',
+			'\nSuggested buildManifest : ${taskName}.wcs,"sourceFile"',
 		defaultCompressLevel: 5,
 	},
 ];

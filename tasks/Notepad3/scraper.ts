8b9b2c2abbf0eb87da0cf0e3972cbce4d842b54c
undefined <34956162+XiaotianDong@users.noreply.github.com>
2022-02-08 16:30:19 +0800

    add Listen1 (#23)
    * add Listen1
    
    * Delete proxy settings
    
    * add Motrix
    
    * Delete axios proxy settings
    
    Motrix
    
    * add Notepad3
    
    * Remove axios proxy setting in Notepad3

---
+++ tasks/Notepad3/scraper.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+let version: string, url: string
+
+
+async function init() {
+    let resp = await axios.get("http://api.github.com/repos/rizonesoft/Notepad3/releases/latest")
+    let respData = resp.data
+    version = respData.tag_name.replace("RELEASE_","")
+    for(let asset of respData['assets']){
+        if(asset.name.endsWith("_x64.zip")){
+            url = asset.browser_download_url
+            break;
+        }
+    }
+}
+
+function getVersion(): string {
+    return version
+}
+
+function getDownloadLink(): string {
+    return url
+}
+
+
+
+export {
+    init, getVersion, getDownloadLink
+}

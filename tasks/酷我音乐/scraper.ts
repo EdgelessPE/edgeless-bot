4360bbaea7b129376254329f6613bac60c9062d9
Cno <dsyourshy@qq.com>
2022-02-13 01:25:14 +0800

    修复酷我

--- tasks/酷我音乐/scraper.ts
+++ tasks/酷我音乐/scraper.ts
@@ -3,7 +3,7 @@ import {ScraperReturned} from '../../src/class';
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
 	return new Ok({
-		version: 'https://down.kuwo.cn/mbox/kwmusic_web_4.exe',
-		downloadLink: '0',
+		downloadLink: 'https://down.kuwo.cn/mbox/kwmusic_web_4.exe',
+		version: '0',
 	});
 }

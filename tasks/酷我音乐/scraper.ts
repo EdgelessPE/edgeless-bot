dff6db1e5a8eb2049fe1a9eeb60da8429fc8be0a
Cno <dsyourshy@qq.com>
2022-02-13 01:26:41 +0800

    修复版本号匹配问题

--- tasks/酷我音乐/scraper.ts
+++ tasks/酷我音乐/scraper.ts
@@ -4,6 +4,6 @@ import {ScraperReturned} from '../../src/class';
 export default async function (): Promise<Result<ScraperReturned, string>> {
 	return new Ok({
 		downloadLink: 'https://down.kuwo.cn/mbox/kwmusic_web_4.exe',
-		version: '0',
+		version: '0.0.0',
 	});
 }

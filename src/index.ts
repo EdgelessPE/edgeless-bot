874d2c52f258fe3753271ba1f1f34665717fd2a1
Cno <dsyourshy@qq.com>
2022-02-14 17:00:20 +0800

    增加移植校验代码

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ import {initAria2c, stopAria2c} from './aria2c';
 import {readDatabase, report, setDatabaseNodeFailure, setDatabaseNodeSuccess, writeDatabase} from './database';
 import {uploadToRemote} from './rclone';
 import art from './art';
+import fs from 'fs';
+import path from 'path';
+import * as TOML from 'toml';
 
 require('source-map-support').install();
 
@@ -73,14 +76,64 @@ async function main(): Promise<boolean> {
 	return report();
 }
 
+interface TaskTemp {
+	name: string;
+	author: string;
+	category: string;
+}
+
 async function test(): Promise<boolean> {
-	console.log(requiredKeysValidator({
-		a: {
-			b: {
-				c: '114',
-			},
-		},
-	}, ['a.b.d']));
+	const oldTasksDir = 'D:\\Desktop\\Projects\\EdgelessPE\\edgeless-bot-master\\tasks',
+		newTasksDir = 'D:\\Desktop\\Projects\\EdgelessPE\\edgeless-bot\\tasks';
+	//读取两侧文件夹
+	let o = fs.readdirSync(oldTasksDir),
+		n = fs.readdirSync(newTasksDir);
+	//读取旧任务
+	let oTasks: TaskTemp[] = [],
+		tmp;
+	for (let taskName of o) {
+		tmp = JSON.parse(fs.readFileSync(path.join(oldTasksDir, taskName, 'config.json')).toString());
+		oTasks.push(tmp);
+	}
+	//读取新任务
+	let nTasks: TaskTemp[] = [];
+	for (let taskName of n) {
+		tmp = TOML.parse(fs.readFileSync(path.join(newTasksDir, taskName, 'config.toml')).toString());
+		nTasks.push(tmp.task);
+	}
+	const getNode = (taskName: string, list: TaskTemp[]): TaskTemp | null => {
+		let r = null;
+		for (let n of list) {
+			if (n.name == taskName) {
+				r = n;
+				break;
+			}
+		}
+		return r;
+	};
+	let m;
+	//检查移植遗漏
+	for (let n of oTasks) {
+		m = getNode(n.name, nTasks);
+		if (m == null) {
+			log(`Warning:Missing ${n.name}`);
+		}
+	}
+	//检查对应
+	for (let n of nTasks) {
+		m = getNode(n.name, oTasks);
+		if (m == null) {
+			log(`Info:New task ${n.name}`);
+		} else {
+			if (n.author != m.author) {
+				log(`Warning:Author not match ${m.author}->${n.author},task ${n.name}`);
+			}
+			if (n.category != m.category) {
+				log(`Error:Category not match ${m.author}->${n.author},task ${n.name}`);
+			}
+		}
+	}
+
 	return true;
 }
 

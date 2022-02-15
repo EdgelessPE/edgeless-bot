27c82536936c3636f0d51d002cb045f6c10ddc70
Cno <dsyourshy@qq.com>
2022-02-15 16:17:07 +0800

    更新任务

--- tasks/酷我音乐/scraper.ts
+++ tasks/酷我音乐/scraper.ts
@@ -1,7 +1,9 @@
 import {Ok, Result} from 'ts-results';
 import {ScraperReturned} from '../../src/class';
+import {sleep} from '../../src/utils';
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
+	await sleep(1000);
 	return new Ok({
 		downloadLink: 'https://down.kuwo.cn/mbox/kwmusic_web_4.exe',
 		version: '0.0.0',

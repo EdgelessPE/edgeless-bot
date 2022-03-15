6b422739fe551d3bce01d68e75ba337ed99de5d6
undefined <34956162+XiaotianDong@users.noreply.github.com>
2022-03-15 20:48:26 +0800

    添加Nali (#35)
    * add Scoop scraper Template
    
    * add nail

--- templates/scrapers/_register.ts
+++ templates/scrapers/_register.ts
@@ -1,4 +1,4 @@
-import {ScraperRegister} from '../../src/class';
+import { ScraperRegister } from "../../src/class";
 
 const regArray: Array<ScraperRegister> = [
   {
@@ -14,31 +14,38 @@ const regArray: Array<ScraperRegister> = [
     requiredKeys: [],
   },
   {
-    name: 'Global Page Match',
-    entrance: 'Global_Page_Match',
-    urlRegex: 'universal://',
+    name: "Global Page Match",
+    entrance: "Global_Page_Match",
+    urlRegex: "universal://",
     requiredKeys: [],
-    description: 'Use given regex to match text in html file, narrow the scope by specify jQuery selector in "scraper_temp.download_selector" "scraper_temp.version_selector"',
+    description:
+      'Use given regex to match text in html file, narrow the scope by specify jQuery selector in "scraper_temp.download_selector" "scraper_temp.version_selector"',
   },
   {
-    name: 'REST API',
-    entrance: 'REST_API',
-    urlRegex: 'universal://',
+    name: "REST API",
+    entrance: "REST_API",
+    urlRegex: "universal://",
     requiredKeys: [
-      'scraper_temp.api_url',
-      'scraper_temp.version_path',
-      'scraper_temp.download_path',
+      "scraper_temp.api_url",
+      "scraper_temp.version_path",
+      "scraper_temp.download_path",
     ],
     description:
-        'Specify Json REST api url by "scraper_temp.api_url", and specify object path by "scraper_temp.version_path" "scraper_temp.download_path"',
+      'Specify Json REST api url by "scraper_temp.api_url", and specify object path by "scraper_temp.version_path" "scraper_temp.download_path"',
   },
   {
-    name: 'Redirection Parse',
-    entrance: 'Redirection_Parse',
-    urlRegex: 'universal://',
-    requiredKeys: ['scraper_temp.redirection_url'],
+    name: "Redirection Parse",
+    entrance: "Redirection_Parse",
+    urlRegex: "universal://",
+    requiredKeys: ["scraper_temp.redirection_url"],
     description:
-        'Parse redirection url to resolve version and direct download link, specify url by "scraper_temp.redirection_url"',
+      'Parse redirection url to resolve version and direct download link, specify url by "scraper_temp.redirection_url"',
+  },
+  {
+    name: "Scoop",
+    entrance: "Scoop",
+    urlRegex: "https?://scoop.sh/[^/]",
+    requiredKeys: ["scraper_temp.bucketName"],
   },
   {
     name: "Scoop",

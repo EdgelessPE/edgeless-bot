639e39c72dbfc70ff7ae0fc8ac15890e8bca2980
Cno <dsyourshy@qq.com>
2022-03-21 22:16:31 +0800

    增加一批bot化的任务

---
+++ tasks/阿里云盘/scraper.ts
@@ -0,0 +1,9 @@
+import {Ok, Result} from 'ts-results';
+import {ScraperReturned} from '../../src/class';
+
+export default async function (): Promise<Result<ScraperReturned, string>> {
+	return new Ok({
+		version: '0.0.0',
+		downloadLink: 'https://yunpan.aliyun.com/downloads/apps/desktop/aDrive.exe',
+	});
+}

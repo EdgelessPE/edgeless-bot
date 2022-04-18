7aabb9e426caa8f0fd920e0779c72d1d82c5aa66
Cno <dsyourshy@qq.com>
2022-04-18 15:54:08 +0800

    完善制作器模板

--- scripts/templates/producer.ts
+++ scripts/templates/producer.ts
@@ -1,6 +1,8 @@
 import {ProducerParameters, ProducerReturned} from '../../src/class';
-import {Ok, Result} from 'ts-results';
+import {writeGBK} from "../../src/utils";
+import {Err, Ok, Result} from 'ts-results';
 import path from 'path';
+import fs from "fs";
 
 const shell = require('shelljs');
 
@@ -8,17 +10,32 @@ interface RequiredObject {
 }
 
 export default async function (p: ProducerParameters): Promise<Result<ProducerReturned, string>> {
-	const {taskName, downloadedFile, workshop, version} = p;
-	const obj = p.requiredObject as RequiredObject;
+    const {taskName, downloadedFile, workshop, version} = p;
+    const obj = p.requiredObject as RequiredObject;
 
-	//Create ready directory
-	const contentDir = path.join(workshop, '_ready', taskName);
-	shell.mkdir('-p', contentDir);
+    //Create ready directory
+    const contentDir = path.join(workshop, '_ready', taskName);
+    shell.mkdir('-p', contentDir);
 
-	//YOUR CODE HERE
+    //YOUR CODE HERE
 
-	//Return ready directory
-	return new Ok({
-		readyRelativePath: '_ready',
-	});
+    //Write command to external batch
+    const cmd = `LINK X:\\Users\\Default\\Desktop\\${taskName},%ProgramFiles%\\Edgeless\\${taskName}\\${taskName}.exe`
+    writeGBK(path.join(workshop, '_ready', taskName + ".wcs"), cmd)
+
+    //Naive self check
+    const manifest = [
+        `${taskName}.wcs`,
+        `${taskName}/${taskName}.exe`
+    ].map((file) => path.join(workshop, '_ready', file))
+    for (let item of manifest) {
+        if (!fs.existsSync(item)) {
+            return new Err(`Error:Self check failed : missing ${item} in ready directory`)
+        }
+    }
+
+    //Return ready directory
+    return new Ok({
+        readyRelativePath: '_ready',
+    });
 }

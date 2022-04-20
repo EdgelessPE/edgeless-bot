430a5152baa81e5e98bb3a93b2b3f6122a6b319b
Cno <dsyourshy@qq.com>
2022-04-20 18:07:59 +0800

    增加分区助手

---
+++ tasks/分区助手/scraper.ts
@@ -0,0 +1,9 @@
+import {Ok, Result} from 'ts-results';
+import {ScraperReturned} from '../../src/class';
+
+export default async function (): Promise<Result<ScraperReturned, string>> {
+	return new Ok({
+		downloadLink: 'https://www2.aomeisoftware.com/download/pacn/PAWinPEx64.7z',
+		version: '0.0.0',
+	});
+}

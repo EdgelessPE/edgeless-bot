a7a81b7e703b5bfb368d3bd67251c4f5ec853d04
undefined <34956162+undefined-ux@users.noreply.github.com>
2022-08-06 02:39:42 +0800

    Fix Chrome Driver & Add DreamScene2 (#60)

--- tasks/DreamScene2/producer.ts
+++ tasks/DreamScene2/producer.ts
@@ -11,14 +11,17 @@ const shell = require('shelljs');
 export default async function (p: ProducerParameters): Promise<Result<ProducerReturned, string>> {
 	const {taskName, downloadedFile, workshop} = p;
 
-
 	const readyDir = path.join(workshop, '_ready', taskName);
-	shell.mkdir('-p', readyDir);
+	shell.mkdir("-p",path.join(workshop, '_ready'))
 	log(`Info: ${downloadedFile}`)
-	release(path.join(workshop, downloadedFile), path.join(workshop, "_ready", taskName))
+	release(path.join(workshop, downloadedFile), workshop)
+	shell.mv(
+		path.join(workshop, downloadedFile.split(".")[0]), 
+		readyDir
+		)
 	writeGBK(
-		path.join(workshop, "_ready", taskName + ".cmd"),
-		`setx Path "%PATH%;X:\\Program Files\\Edgeless\\${taskName}"`
+		path.join(workshop, "_ready", taskName + ".wcs"),
+		`LINK %desktop%\\DreamScene2 X:\\Program Files\\Edgeless\\${taskName}\\DreamScene2.exe"`
 		)
 	//Return ready directory
 	return new Ok({

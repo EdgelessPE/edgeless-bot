bf8be1eb10cbaf0b4f8f807aa46c15348413150d
L0serj3rry <dsyourshy@qq.com>
2023-07-02 17:02:34 +0800

    fix:修复酷我音乐

--- tasks/酷我音乐/scraper.ts
+++ tasks/酷我音乐/scraper.ts
@@ -3,7 +3,7 @@ import { ScraperReturned } from "../../src/types/class";
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
   return new Ok({
-    downloadLink: "https://down.kuwo.cn/mbox/kwmusic_web_4.exe",
+    downloadLink: "https://down.kuwo.cn/mbox/kwmusic_web_6.exe",
     version: "0.0.0",
   });
 }

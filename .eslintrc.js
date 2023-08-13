b075818d157f8b11b8b68b8caba0d0439e2488ed
L0serj3rry <dsyourshy@qq.com>
2023-08-13 22:45:11 +0800

    chore:修复eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        ".eslintrc.{js,cjs}",
-        "src/**/*.ts",
-        "templates/**/*.ts",
-        "tasks/**/*.ts",
-        "scripts/*.ts",
-      ],
-      parserOptions: {
-        sourceType: "script",
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
-  ignorePatterns: ["*.js", "/dist", "/workshop", "/scripts/templates"],
-};
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}",
+                "src/**/*.ts",
+                "templates/**/*.ts",
+                "tasks/**/*.ts",
+                "scripts/*.ts",
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "prettier"
+    ],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "prettier/prettier": "error"
+    },
+    "ignorePatterns": ["*.js", "/dist", "/workshop", "/scripts/templates"],
+}

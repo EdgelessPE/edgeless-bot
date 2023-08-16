1377932db6d404402190dd3bf6ae701e910e127a
L0serj3rry <dsyourshy@qq.com>
2023-08-16 21:23:13 +0800

    chore:优化getBadge

--- src/badge.ts
+++ src/badge.ts
@@ -1,78 +1,30 @@
 import chalk from "chalk";
 
-const base = Math.floor(Math.random() * 14);
+const choices = [
+  chalk.bgRed,
+  chalk.bgGreen,
+  chalk.bgYellow,
+  chalk.bgBlue,
+  chalk.bgMagenta,
+  chalk.bgCyan,
+  chalk.bgWhite,
+  chalk.bgGray,
+  chalk.bgRedBright,
+  chalk.bgGreenBright,
+  chalk.bgYellowBright,
+  chalk.bgBlueBright,
+  chalk.bgMagentaBright,
+  chalk.bgCyanBright,
+];
+const base = Math.floor(Math.random() * choices.length);
 let count = 0;
 
 export function getBadge(type: string): string {
-  const choices = [
-      chalk.bgRed,
-      chalk.bgGreen,
-      chalk.bgYellow,
-      chalk.bgBlue,
-      chalk.bgMagenta,
-      chalk.bgCyan,
-      chalk.bgWhite,
-      chalk.bgGray,
-      chalk.bgRedBright,
-      chalk.bgGreenBright,
-      chalk.bgYellowBright,
-      chalk.bgBlueBright,
-      chalk.bgMagentaBright,
-      chalk.bgCyanBright,
-    ],
-    res = (choices[(base + count) % 14] ?? chalk.bgWhiteBright)(
-      type + " " + count,
-    );
+  const res = (choices[(base + count) % choices.length] ?? chalk.bgWhiteBright)(
+    type + " " + count,
+  );
 
   count++;
 
   return res;
 }
-
-// switch ((base + count) % 14) {
-//	case 0:
-//		res = chalk.bgRed(type + ' ' + count);
-//		break;
-//	case 1:
-//		res = chalk.bgGreen(type + ' ' + count);
-//		break;
-//	case 2:
-//		res = chalk.bgYellow(type + ' ' + count);
-//		break;
-//	case 3:
-//		res = chalk.bgBlue(type + ' ' + count);
-//		break;
-//	case 4:
-//		res = chalk.bgMagenta(type + ' ' + count);
-//		break;
-//	case 5:
-//		res = chalk.bgCyan(type + ' ' + count);
-//		break;
-//	case 6:
-//		res = chalk.bgWhite(type + ' ' + count);
-//		break;
-//	case 7:
-//		res = chalk.bgGray(type + ' ' + count);
-//		break;
-//	case 8:
-//		res = chalk.bgRedBright(type + ' ' + count);
-//		break;
-//	case 9:
-//		res = chalk.bgGreenBright(type + ' ' + count);
-//		break;
-//	case 10:
-//		res = chalk.bgYellowBright(type + ' ' + count);
-//		break;
-//	case 11:
-//		res = chalk.bgBlueBright(type + ' ' + count);
-//		break;
-//	case 12:
-//		res = chalk.bgMagentaBright(type + ' ' + count);
-//		break;
-//	case 13:
-//		res = chalk.bgCyanBright(type + ' ' + count);
-//		break;
-//	default:
-//		res = chalk.bgWhiteBright(type + ' ' + count);
-//		break;
-// }

634f35ef530e89aa6735df446fcce87499ad45d7
Cno <dsyourshy@qq.com>
2024-03-31 14:43:09 +0800

    task: update uTools

--- tasks/uTools/scraper.ts
+++ tasks/uTools/scraper.ts
@@ -1,6 +1,7 @@
 import { Ok, Result } from "ts-results";
 import { ScraperReturned } from "../../src/class";
 import { robustGet } from "../../src/network";
+import { matchVersion } from "../../src/utils";
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
   // 请求官网
@@ -8,13 +9,11 @@ export default async function (): Promise<Result<ScraperReturned, string>> {
   // 匹配publishURL
   const pum = page.match(/publishURL\s*=\s*'http\S*/) as RegExpMatchArray;
   const publishURL = pum[0].split("'")[1];
-  // 匹配package
-  const pm = page.match(/uTools-\d*\.\d*\.\d*\.exe/) as RegExpMatchArray;
-  const name = pm[0];
-  const downloadLink = publishURL + name;
   // 匹配版本号
-  const vm = name.match(/\d*\.\d*\.\d*/) as RegExpMatchArray;
-  const version = vm[0];
+  const vm = page.match(/version\s*=\s*'\d*\.\d*\.\d*'/) as RegExpMatchArray;
+  const version = matchVersion(vm[0]).unwrap();
+  // 拼接下载链接
+  const downloadLink = publishURL + "uTools-" + version + ".exe";
 
   return new Ok({
     version,

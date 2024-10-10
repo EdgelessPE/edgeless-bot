b3465e1bd1abb8b403d3b6f7bcc9edc88431de03
xrgzs <xiaoran@xrgzs.top>
2024-10-10 15:51:05 +0800

    fix: update youku version retrieval method

--- tasks/优酷/scraper.ts
+++ tasks/优酷/scraper.ts
@@ -1,26 +1,15 @@
-import { Ok, Err, Result } from "ts-results";
+import { Ok, Result } from "ts-results";
 import { ScraperReturned } from "../../src/class";
 import { robustGet } from "../../src/network";
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
-  const page = (
-    await robustGet("https://youku.com/product/index")
-  ).unwrap() as string;
-
-  // 匹配初始化数据
-  const jsonTextMatch = page.match(/window\.__INITIAL_DATA__\s*=\s*([^;]+);/g);
-  if (jsonTextMatch == null) return new Err("Error:Can't match initial data");
-  const json = JSON.parse(
-    jsonTextMatch[0].split(/window.__INITIAL_DATA__\s*=\s*/)[1].slice(0, -1),
-  );
-  // console.log(JSON.stringify(json,null,2))
-
-  // 选中pc版
-  const node = json[0].subNav[0];
-  // console.log(JSON.stringify(node,null,2))
-
+  const prefix = "https://pcclient.download.youku.com/iku-win-release/";
+  const versionUrl = `${prefix}latest.yml?noCache=${Math.random()}`;
+  const versionInfo: any = (await robustGet(versionUrl)).unwrap() as string;
+  const version = versionInfo.match(/version: (.+)/)[1];
+  const downloadLink = prefix + versionInfo.match(/url: (.+)/)[1];
   return new Ok({
-    version: node.ver,
-    downloadLink: node.downloadUrl,
+    version,
+    downloadLink,
   });
 }

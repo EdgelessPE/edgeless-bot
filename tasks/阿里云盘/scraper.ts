b076ac53bff4eabe54e57f4f76a3d7fc7f0e4650
xrgzs <xiaoran@xrgzs.top>
2024-10-10 15:09:20 +0800

    fix: aDrive implement dynamic version and download link retrieval

--- tasks/阿里云盘/scraper.ts
+++ tasks/阿里云盘/scraper.ts
@@ -1,10 +1,21 @@
 import { Ok, Result } from "ts-results";
 import { ScraperReturned } from "../../src/class";
+import { robustGet } from "../../src/network";
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
+  const versionApi: any = (
+    await robustGet("https://www.aliyundrive.com/desktop/version/update.json", {
+      responseType: "json",
+    })
+  ).unwrap();
+  const versionUrl = `${
+    versionApi.url
+  }/win32/x64/latest.yml?noCache=${Math.random()}`;
+  const versionInfo: any = (await robustGet(versionUrl)).unwrap() as string;
+  const version = versionInfo.match(/version: (.+)/)[1];
+  const downloadLink = versionInfo.match(/url: (.+)/)[1];
   return new Ok({
-    version: "0.0.0",
-    downloadLink:
-      "https://cdn.aliyundrive.net/downloads/apps/desktop/aDrive.exe",
+    version,
+    downloadLink,
   });
 }

ad013f98dbb5369f5403716ab3fb733024b20949
dongheming <dongheming@bytedance.com>
2026-04-27 21:59:01 +0800

    fix: 搜狗拼音
    更新下载链接正则以匹配新域名 ime.sogouimecdn.com，并改为从 URL 直接提取版本号（原 changelog.php 返回 GBK 编码导致解析失败）

--- tasks/搜狗拼音/scraper.ts
+++ tasks/搜狗拼音/scraper.ts
@@ -1,18 +1,16 @@
 import { Err, Ok, Result } from "ts-results";
 import { ScraperReturned } from "../../src/class";
 import { robustGet } from "../../src/network";
-import { log, versionCmp, Cmp } from "../../src/utils";
+import { log } from "../../src/utils";
 
-const reg =
-  /https:\/\/ime-sec.gtimg.com\/[0-9]+\/\w+\/pc\/dl\/gzindex\/[0-9]+\/sogou_pinyin_[\w.]+.exe/;
+const reg = /https?:\/\/ime\.sogouimecdn\.com\/[^\s"'<>]+\.exe/;
 
 export default async function (): Promise<Result<ScraperReturned, string>> {
-  let version = "0.0";
   // 获取官网首页
   const page = (
     await robustGet("https://pinyin.sogou.com/windows/?r=mac&t=pinyin")
   ).unwrap() as string;
-  // 匹配页面json数据
+  // 匹配下载链接
   const m = page.match(reg);
   if (m == null) {
     return new Err("Error:Can't match url");
@@ -20,22 +18,12 @@ export default async function (): Promise<Result<ScraperReturned, string>> {
   const url = m[0];
   log(`Info:Matched url ${url}`);
 
-  // 获取升级日志页面
-  const versionPage = (
-    await robustGet("https://pinyin.sogou.com/changelog.php")
-  ).unwrap() as string;
-  // 匹配所有的正式版发布信息
-  // console.log(gb2312(page.data))
-  const matches = versionPage.match(/\d*\.\d*\S*<\/h2>/) as RegExpMatchArray;
-  // 获取其中最高的版本号
-  let match;
-  matches.forEach((item) => {
-    // 提取版本号
-    match = item.match(/\d*\.\d*/);
-    if (match && versionCmp(version, match[0]) == Cmp.L) {
-      version = match[0];
-    }
-  });
+  // 从 URL 中提取版本号
+  const versionMatch = url.match(/sogou_pinyin_([\d.]+)\.exe/);
+  if (versionMatch == null) {
+    return new Err("Error:Can't match version from url");
+  }
+  const version = versionMatch[1];
   log(`Info:Matched version ${version}`);
 
   return new Ok({
